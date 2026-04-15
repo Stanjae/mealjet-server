53e3168d876ce0628b7c1c57d44149eef89b989a
stanjae <stanykhay29@gmail.com>
2026-04-15 16:40:18 +0100

    feat: allow - localhost

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ export function createApp(): Application {
   app.use(helmet());
   app.use(
     cors({
-      origin: env.CLIENT_URL,
+      origin: [env.CLIENT_URL, 'http://localhost:3000'],
       credentials: true, // Allow cookies (refresh token)
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"],
