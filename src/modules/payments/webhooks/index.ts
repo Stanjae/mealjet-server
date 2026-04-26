3cc27871cb34bac926da7345161c96cce87dd0b6
stanjae <stanykhay29@gmail.com>
2026-04-26 14:14:22 +0100

    chore: debug webhook

--- src/modules/payments/webhooks/index.ts
+++ src/modules/payments/webhooks/index.ts
@@ -10,11 +10,15 @@ export const handlePaystackWebhook = async (req: Request, res: Response) => {
     .update(req.body)
     .digest("hex");
 
+    console.log("Received Paystack webhook, verifying signature...", hash);
+
   if (hash !== req.headers["x-paystack-signature"]) {
+    console.log("Invalid Paystack webhook signature");
     return res.status(401).send("Invalid signature");
   }
 
   // STEP B: Respond to Paystack immediately (within 5 seconds)
+  console.log("Received webhook from Paystack, responding with 200 OK", req.headers);
   res.sendStatus(200);
 
   // STEP C: Process the event asynchronously
@@ -22,13 +26,16 @@ export const handlePaystackWebhook = async (req: Request, res: Response) => {
 
   try {
     if (event === "charge.success") {
+      console.log("Received charge.success webhook from Paystack");
       await paymentService.handlePaymentSuccess(req, data);
     }
 
     if (event === "charge.failed") {
+      console.log("Received charge.failed webhook from Paystack");
       await paymentService.handlePaymentFailed(req, data);
     }
   } catch (err) {
     logger.error(err);
+    console.log("an error occurred",err);
   }
 };
